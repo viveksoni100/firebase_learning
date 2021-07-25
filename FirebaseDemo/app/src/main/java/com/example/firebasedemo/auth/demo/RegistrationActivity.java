36c8442ddc71620d558f0af964e01b9abcf18f01
viveksoni100 <viveksoni100@gmail.com>
2021-07-25 18:41:21 +0530

    registration using firebase auth is enabled

---
+++ FirebaseDemo/app/src/main/java/com/example/firebasedemo/auth/demo/RegistrationActivity.java
@@ -0,0 +1,62 @@
+package com.example.firebasedemo.auth.demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.firebasedemo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button register;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser(email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    public void registerUser(String email, String password) {
+        try {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                    RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(RegistrationActivity.this, "Done...!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegistrationActivity.this, "Locha paida...!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
